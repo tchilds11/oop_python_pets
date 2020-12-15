32ddb49fee5da7c9f2d0319cdd4d1b14d5882c1e
Terrence Childs <tchilds1108@gmail.com>
2020-12-15 15:08:23 -0500

    second

--- Contact.py
+++ Contact.py
@@ -6,5 +6,6 @@ from person_1 import Person
 
 
 
-contact_info = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
-print(self.name, self.email, self.phone)
+ contact_info = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
+print(contact_info.name, contact_info.email, contact_info.phone)
+
--- person_1.py
+++ person_1.py
@@ -3,13 +3,26 @@ class Person:
         self.name = name
         self.email = email
         self.phone = phone
+        self.greeting_count = 0
+        self.friend = friend
+
 
     def greet(self, other_person):
         print('Hello {}, I am {}!'.format(other_person.name, self.name))
+        self.greeting_count +=1
 
-Sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
-Jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
+    def add_friend(self, new_friend):
+        self.friend.append(new_friend)
+
+    def num_friend(self):
+        (print(len(self.friend))
+
+# Sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
+# Jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
 
 
-Sonny.greet(Jordan)
-Jordan.greet(Sonny)
+
+
+
+# Sonny.greet(Jordan)
+# Jordan.greet(Sonny)
